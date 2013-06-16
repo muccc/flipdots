7c30d1009b13b8da9c78c1c488116fb8c3fae277
Tobias Schneider <schneider@blinkenlichts.net>
2013-06-16 18:14:39 +0200

    wrong file...

--- testsender.py
+++ testsender.py
@@ -1,10 +1,9 @@
 import socket
 import time
+import sys
 
-#UDPHOST="2001:a60:2458:f001:222:f9ff:fe01:c65"
-UDPHOST="fe80::222:f9ff:fe01:c65%wlan0"
+UDPHOST="fe80::222:f9ff:fe01:c65%eth0"
 UDPPORT=2323
-UDPPORT=2704
 
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 
@@ -12,8 +11,5 @@ def send(value):
   msg = chr(value)*((16*40)/8)
   sock.sendto(msg, (UDPHOST, UDPPORT))
 
-v = 0
-while 1:
-    send(v)
-    v+=1
-    time.sleep(.4)
+v = int(sys.argv[1])
+send(v)
