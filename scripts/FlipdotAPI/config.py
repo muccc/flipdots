d72b9dd76854069637d1944bd42362c385417328
Markus Mueller <john.subscriber@markus.institute>
2017-03-03 14:48:16 +0000

    FlipdotAPI: remove all the API

--- scripts/FlipdotAPI/config.py
+++
@@ -1,9 +0,0 @@
-panelAddressArray = [
-        ("2001:7f0:3003:235e:ba27:ebff:feb9:db12",2323),
-        ("2001:7f0:3003:235e:ba27:ebff:fe23:60d7", 2323),
-        ("2001:7f0:3003:235e:ba27:ebff:fe71:dd32", 2323)
-        ]
-
-zeileAddressArray = [
-        ("2001:7f0:3003:beef:ba27:ebff:fe89:4cd2", 2323)
-        ]
