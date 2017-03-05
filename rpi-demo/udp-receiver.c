f125fa6487b5697ee3320732622ec759c1c0130f
schneider <schneider@blinkenlichts.net>
2017-03-05 16:17:06 +0000

    feat(udp-receiver): Catch CTRL+C

--- rpi-demo/udp-receiver.c
+++ rpi-demo/udp-receiver.c
@@ -3,17 +3,34 @@
 #include "config.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <signal.h>
+#include <stdlib.h>
+
+static bool keepRunning = true;
+
+void intHandler(int x) {
+    keepRunning = false;
+}
 
 extern int active_pinning;
 int main(void)
 {
     bool init = false;
+
+    struct sigaction act;
+    act.sa_handler = intHandler;
+    sigaction(SIGINT, &act, NULL);
+
     flipdot_net_init();
     flipdot_init();
     
     uint8_t data[CONFIG_BUS_COUNT][MODULE_BYTE_COUNT * CONFIG_BUS_LENGTH];
-    while (1) {
+    while (keepRunning) {
         int n = flipdot_net_recv_frame((uint8_t *)data, sizeof(data));
+        if(n <= 0) {
+            break;
+        }
+
         if(!init) {
             init=true;
             flipdot_init();
