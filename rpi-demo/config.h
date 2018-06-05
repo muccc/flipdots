b73b949e92033136a53946abdb0784172e13ce10
schneider <schneider@blinkenlichts.net>
2018-06-05 22:24:29 +0200

    feat(rpi-demo): Simplify config

--- rpi-demo/config.h
+++ rpi-demo/config.h
@@ -1,14 +1,20 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
-#define CONFIG_BUS_LENGTH       (18 * 3)
-// The number of panels on the bus.
+#define PANELSX     9
+#define PANELSY     6
 
+
+// The number of panels on the bus.
+#define CONFIG_BUS_LENGTH       (PANELSX * PANELSY)
+
+// Physical dimension (One long string going up)
 #define XP  16
 #define YP  (20 * CONFIG_BUS_LENGTH)
 
-#define XL  (16 * 9)
-#define YL  (20 * 6)
+// Logical dimension (panels in a matrix configuration)
+#define XL  (16 * PANELSX)
+#define YL  (20 * PANELSY)
 #define LEFT_TO_RIGHT   1
 
 #define INVERT_GPIO  0
